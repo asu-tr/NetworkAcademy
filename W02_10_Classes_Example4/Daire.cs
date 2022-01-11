4b8fd6073eb9619d5bfac3b342bfdd4a91b2612a
NA-403 <403_SABAH@DESKTOP-M3J5PF2>
2022-01-11 13:43:49 +0300

    W02_10_Classes_Example4

---
+++ W02_10_Classes_Example4/Daire.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W02_10_Classes_Example4
+{
+    public class Daire
+    {
+        public double yaricap;
+        private double pisayisi;
+
+        public Daire()
+        {
+            pisayisi = 3;
+        }
+        ~Daire()
+        {
+            //Yıkıcı metot
+        }
+        public double AlanHesapla(double yaricapdisardangelen)
+        {
+            double alan = pisayisi * Math.Pow(yaricapdisardangelen, 2);
+            return alan;
+        }
+
+        public double CevreHesapla(double yaricap)
+        {
+            return 2 * yaricap * pisayisi;
+        }
+    }
+}
---
+++ W02_10_Classes_Example4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W02_10_Classes_Example4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //double pi=Math.PI;
+            //pi = Math.Round(pi,2);
+            //Console.WriteLine(pi);
+
+            Daire daire = new Daire();
+
+            Console.Write("Dairenin yarıçapını giriniz:");
+            daire.yaricap = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Dairenin alanı:" + daire.AlanHesapla(daire.yaricap));
+
+            Console.WriteLine("Dairenin çevresi:" + daire.CevreHesapla(daire.yaricap));
+            Console.ReadLine();
+        }
+    }
+}
