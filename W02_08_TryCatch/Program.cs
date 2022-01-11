3f2ccf82edb290cfec879da8c4db6b330b9596f4
NA-403 <403_SABAH@DESKTOP-M3J5PF2>
2022-01-11 13:33:36 +0300

    W02_08_TryCatch

---
+++ W02_08_TryCatch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W02_08_TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 20;
+            int sayi2 = 0;
+            int bolum = 0;
+
+            try
+            {
+                bolum = sayi1 / sayi2;
+                Console.WriteLine(bolum);
+                // Hata yoksa bu kısım çalışır
+            }
+            //catch (Exception hata)
+            //{
+            //    Console.WriteLine(hata.Message);         //Console.WriteLine("Program çalışma esnasında bir hata ile karşılaştı.");
+            //    //Hata alırsa bu kısım çalışır
+            //}
+            //catch (DivideByZeroException hata)
+            //{
+            //    Console.WriteLine(hata.Message);
+            //}
+            catch (FormatException hata)
+            {
+                Console.WriteLine(hata.Message);
+            }
+
+            finally
+            {
+                //Her durumda çalışır.
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
