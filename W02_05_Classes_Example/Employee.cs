9eb05b5258ab51adac201beb75d6024b8426dc9a
NA-403 <403_SABAH@DESKTOP-M3J5PF2>
2022-01-11 10:23:10 +0300

    W02_05_Classes_Example

---
+++ W02_05_Classes_Example/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W02_05_Classes_Example
+{
+    public class Employee
+    {
+        public long EmployeeID;
+        public string Name;
+        public string Surname;
+
+        public void AddToList(List<Employee> list)
+        {
+            list.Add(this);
+        }
+
+        public void RemoveFromList(List<Employee> list)
+        {
+            list.Remove(this);
+        }
+
+        public static void ShowList(List<Employee> list)
+        {
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.EmployeeID);
+                Console.WriteLine(emp.Name);
+                Console.WriteLine(emp.Surname + "\n");
+            }
+        }
+    }
+}
---
+++ W02_05_Classes_Example/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W02_05_Classes_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Employee e = new Employee();
+            e.EmployeeID = 1101069;
+            e.Name = "Lauren";
+            e.Surname = "Wills";
+
+            e.AddToList(employees);
+
+            Employee e2 = new Employee();
+            e2.EmployeeID = 1101070;
+            e2.Name = "Jorge";
+            e2.Surname = "Matias";
+
+            e2.AddToList(employees);
+
+            employees.Add(new Employee() { EmployeeID = 1101071, Name = "Billy", Surname = "Shaw" });
+
+            Employee.ShowList(employees);
+
+            e2.RemoveFromList(employees);
+
+            Employee.ShowList(employees);
+
+            Console.ReadLine();
+        }
+    }
+}
