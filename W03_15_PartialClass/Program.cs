5d790885912417daadfc13a4dab3a9f937e34894
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-01-12 15:04:24 +0300

    W03_15_PartialClass

---
+++ W03_15_PartialClass/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W03_15_PartialClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.FirstName = "sth";
+            s1.Add();
+        }
+    }
+}
---
+++ W03_15_PartialClass/Student2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W03_15_PartialClass
+{
+    public partial class Student
+    {
+        public void Add()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+
+        public void Edit()
+        {
+
+        }
+        public void List()
+        {
+
+        }
+    }
+}
