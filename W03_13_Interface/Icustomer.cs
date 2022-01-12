ffd7b94cbcdebb306e9eb82f30a2a849e1909369
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-01-12 13:37:41 +0300

    Revert "Revert "W03_13_Interface""
    This reverts commit bd1e2532d04370d8ab13a3d22815201ce16e0c96.

---
+++ W03_13_Interface/Icustomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W03_13_Interface
+{
+    public interface Icustomer
+    {
+        // no field here, only property
+        int id { get; set; }
+        string firstname { get; set; }
+        string lastname { get; set; }
+
+        // methods cannot have body
+        int newrecord(string first, string last);
+        int deleterecord(int id);
+    }
+}
