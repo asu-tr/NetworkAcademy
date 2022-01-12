558163afafd12d39771651ffc68eb05ee86eff8a
NA-403 <403_SABAH@DESKTOP-M3J5PF2>
2022-01-12 09:58:35 +0300

    W03_09_Enum

---
+++ W03_09_Enum/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W03_09_Enum
+{
+    public enum race
+    {
+        German_Shepherd, Golden, Siberian_Husky, Dachshund
+    }
+    public class Animal
+    {
+        public string ID;
+        public race Race;
+        public string Name;
+    }
+}
---
+++ W03_09_Enum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace W03_09_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.Clear();
+            Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+            Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+            Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.Clear();
+            Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Thread.Sleep(1000);
+
+            Animal dog = new Animal();
+            dog.ID = "01";
+            dog.Name = "Chucky";
+            dog.Race = race.German_Shepherd;
+            
+            Animal dog2 = new Animal();
+            dog.ID = "02";
+            dog.Name = "Karabas";
+            dog.Race = race.Siberian_Husky;
+            dog.Race = (race)2;
+
+            string raceInfo = Enum.GetName(typeof(race), dog.Race);
+            string[] raceArray = Enum.GetNames(typeof(race));
+
+            Console.WriteLine("HEY");
+
+            Console.ReadKey();
+        }
+    }
+}
