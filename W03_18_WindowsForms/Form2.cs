7963937fea848421730e6f56999c9d06f17f7e4f
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-01-13 14:52:35 +0300

    W03_18_WindowsForms

---
+++ W03_18_WindowsForms/Form2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W03_18_WindowsForms
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string str)
+        {
+            InitializeComponent();
+            label1.Text = str;
+        }
+    }
+}
