fe57e4fca751832c634869ccc951b5aeff8ac6cb
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-01-13 14:51:57 +0300

    W03_17_Delegates

---
+++ W03_17_Delegates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace W03_17_Delegates
+{
+    class Program
+    {
+        delegate void Print(string str);
+        static void Main(string[] args)
+        {
+            Print p = new Print(Hello);
+            p += Hi;
+            p += What;
+
+            p.Invoke("Asu");
+
+            Delegate[] methods = p.GetInvocationList();
+
+            foreach (var item in methods)
+            {
+                Console.WriteLine(item.Method.Name);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Hello(string a)
+        {
+            Console.WriteLine("Hello " + a);
+        }
+
+        static void Hi(string a)
+        {
+            Console.WriteLine("Hi " + a);
+        }
+
+        static void What(string a)
+        {
+            Console.WriteLine("What's up " + a);
+        }
+    }
+}
