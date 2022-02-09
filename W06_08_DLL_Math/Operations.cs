89f09c1066afd9f1353376eb1f7f90d5e0a81dcc
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-02-09 14:58:43 +0300

    W06_08_DLL_Math

---
+++ W06_08_DLL_Math/Operations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W06_08_DLL_Math
+{
+    public class Operations
+    {
+        public int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public int Multiply(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+    }
+}
