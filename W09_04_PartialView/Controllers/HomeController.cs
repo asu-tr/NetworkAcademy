166a92eda3ddf35b42040551e76f24f253416527
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-03-02 14:21:00 +0300

    W09_04_PartialView

---
+++ W09_04_PartialView/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace W09_04_PartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
---
+++ W09_04_PartialView/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="W09_04_PartialView.MvcApplication" Language="C#" %>
