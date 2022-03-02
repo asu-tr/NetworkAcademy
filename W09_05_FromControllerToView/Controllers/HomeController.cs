801682a03b30044a6d258f9d9ee71c41d4d22221
asu-tr <29000874+asu-tr@users.noreply.github.com>
2022-03-02 15:04:25 +0300

    W09_05_FromControllerToView

---
+++ W09_05_FromControllerToView/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Mvc;
+
+namespace W09_05_FromControllerToView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["text1"] = "THE TEXT";
+            ViewData["check"] = true;
+
+            ViewBag.text1 = "THE VIEWBAG TEXT";
+            ViewBag.text2 = "TEXT 2";
+
+            TempData["Text3"] = "THE TEXT 3";
+            TempData["Check2"] = false;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.text1 = "THE VIEWBAG TEXT";
+            TempData["text2"] = "TEXT 2";
+
+
+            ViewBag.liste = new SelectListItem[]
+            {
+                new SelectListItem(){Text="one", Value="1"},
+                new SelectListItem(){Text= "two", Value="2"},
+                new SelectListItem(){Text = "three", Value ="3"}
+            };
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.t2t2 = TempData["text2"];
+
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            ViewBag.t3 = TempData["text2"];
+
+            return View();
+        }
+    }
+}
---
+++ W09_05_FromControllerToView/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="W09_05_FromControllerToView.MvcApplication" Language="C#" %>
